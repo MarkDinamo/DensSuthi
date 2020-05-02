094a21d812c48f6b04a2b614f96bc7dcbae898fc
Mark <mdcmark3@gmail.com>
2020-05-02 09:27:47 +0200

    wip

--- PizzaAndSushi.DB/Migrations/20200502071324_PopulateRestourantAddress.cs
+++ PizzaAndSushi.DB/Migrations/20200502071324_PopulateRestourantAddress.cs
@@ -6,7 +6,31 @@ namespace PizzaAndSushi.DB.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.InsertData("Restaurants", new string[] { "Id", "Address" },
+                new object[] {
+                    1,
+                    "Rokossovs'koho St, 16А"
+                });
+            migrationBuilder.InsertData("Restaurants", new string[] { "Id", "Address" },
+                new object[] {
+                    2,
+                    "Myru Ave, 30 А,"
+                });
+            migrationBuilder.InsertData("Restaurants", new string[] { "Id", "Address" },
+                new object[] {
+                    3,
+                    "Kyrponosa St, 34"
+                });
+            migrationBuilder.InsertData("Restaurants", new string[] { "Id", "Address" },
+                new object[] {
+                    4,
+                    "Rokossovs'koho St, 42А"
+                });
+            migrationBuilder.InsertData("Restaurants", new string[] { "Id", "Address" },
+                new object[] {
+                    5,
+                    "Ivana Mazepy St, 55/3"
+                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
---
+++ PizzaAndSushi.DB/Migrations/20200502072158_AddOrderStatuses.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PizzaAndSushi.DB.Migrations
+{
+    public partial class AddOrderStatuses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("OrderStatuses", new string[] { "Id", "Name" },
+               new object[] {
+                    1,
+                    "Created"
+               });
+            migrationBuilder.InsertData("OrderStatuses", new string[] { "Id", "Name" },
+               new object[] {
+                    2,
+                    "Preparing"
+               });
+            migrationBuilder.InsertData("OrderStatuses", new string[] { "Id", "Name" },
+               new object[] {
+                    3,
+                    "Delivering"
+               });
+            migrationBuilder.InsertData("OrderStatuses", new string[] { "Id", "Name" },
+               new object[] {
+                    4,
+                    "Rejected"
+               });
+            migrationBuilder.InsertData("OrderStatuses", new string[] { "Id", "Name" },
+               new object[] {
+                    5,
+                    "Waiting for collect"
+               });
+            migrationBuilder.InsertData("OrderStatuses", new string[] { "Id", "Name" },
+               new object[] {
+                    6,
+                    "Resolved"
+               });
+            migrationBuilder.InsertData("OrderStatuses", new string[] { "Id", "Name" },
+               new object[] {
+                    7,
+                    "Confirmed"
+               });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
