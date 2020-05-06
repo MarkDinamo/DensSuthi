15e4cda6f425bc6b430279f594fcf8d614ec3f09
Mark <mdcmark3@gmail.com>
2020-05-06 20:39:29 +0200

    WIP

--- PizzaAndSushi.WEB/ClientApp/src/components/adminComponents/product-manager.jsx
+++ PizzaAndSushi.WEB/ClientApp/src/components/adminComponents/product-manager.jsx
@@ -1,6 +1,7 @@
 ﻿import React, { useEffect, useState } from 'react';
 import { Table, TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col, CustomInput } from 'reactstrap';
 import { DeleteProduct } from '../adminComponents/productManagerActions/deleteProduct'
+import { CreateOrUpdateProduct } from '../adminComponents/productManagerActions/CreateOrUpdateProduct'
 
 export function ProductManager(props) {
     const [products, setProducts] = useState([]);
@@ -29,35 +30,38 @@ export function ProductManager(props) {
 
     return (
         <div>
-            <Table striped>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Product name</th>
-                        <th>Category</th>
-                        <th>Price</th>
-                        <th>Is hidden</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {products.map((product, index) =>
-                        <tr key={product.id}>
-                            <th scope="row">{index + 1}</th>
-                            <td>{product.name}</td>
-                            <td>
-                                {getCategoryName(product.id)}
-                            </td>
-                            <td>{product.price}</td>
-                            <td><CustomInput  type="checkbox" disabled checked={product.isHidden} /></td>
-                            <td>
-                                <DeleteProduct product={product}></DeleteProduct>
-                            </td>
+            {products.length > 0 && categories.length > 0 &&
+                <Table striped>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Product name</th>
+                            <th>Category</th>
+                            <th>Price</th>
+                            <th>Is hidden</th>
+                            <th>Actions</th>
                         </tr>
-                    )
-                    }
-                </tbody>
-            </Table>
+                    </thead>
+                    <tbody>
+                        {products.map((product, index) =>
+                            <tr key={product.id}>
+                                <th scope="row">{index + 1}</th>
+                                <td>{product.name}</td>
+                                <td>
+                                    {getCategoryName(product.id)}
+                                </td>
+                                <td>{product.price}</td>
+                                <td><CustomInput id={product.id+'isHidden'} type="checkbox" disabled checked={product.isHidden} /></td>
+                                <td>
+                                    <DeleteProduct product={product}></DeleteProduct>
+                                    <CreateOrUpdateProduct categories={categories} product={product}></CreateOrUpdateProduct>
+                                </td>
+                            </tr>
+                        )
+                        }
+                    </tbody>
+                </Table>
+            }
         </div>
     )
 }
---
+++ PizzaAndSushi.WEB/ClientApp/src/components/adminComponents/productManagerActions/CreateOrUpdateProduct.jsx
@@ -0,0 +1,102 @@
+﻿import React, { useEffect, useState } from 'react';
+import { Modal, ModalHeader, ModalFooter, ModalBody, NavItem, NavLink, Card, Button, Form, FormGroup, Label, Input, FormText, CardTitle, CardText, Row, Col, CustomInput } from 'reactstrap';
+
+export function CreateOrUpdateProduct(props) {
+    console.log(props);
+    const [productModel, setProductModel] = useState({});
+    const [isCreate, setIsCreate] = useState(true);
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);
+
+    const openDialog = () => {
+        setModal(true);
+    }
+
+    useEffect(() => {
+        if (props && props.product) {
+            setProductModel(props.product);
+            setIsCreate(false);
+        }
+        else {
+            setProductModel({
+                id: 0,
+                name: "",
+                details: '',
+                price: 0,
+                weight: 0,
+                isHidden: false,
+                isLiquid: false,
+                productTypeId: 0
+            })
+        }
+    }, [])
+
+
+    const onChangeHandler = (property, value) => {
+        console.log(property, value);
+        let copy = { ...productModel };
+        copy[property] = value;
+        setProductModel(copy);
+    }
+    return (
+        <>
+            {
+                isCreate
+                    ? <Button onClick={openDialog} color="primary">Create</Button>
+                    : <Button onClick={openDialog} color="success">Edit</Button>
+            }
+            <Modal isOpen={modal} toggle={toggle}>
+                {
+                    isCreate
+                        ? <ModalHeader toggle={toggle}>Create new product</ModalHeader>
+                        : <ModalHeader toggle={toggle}>Edit product {props.name}</ModalHeader>
+                }
+                <ModalBody>
+                    <Form>
+                        <FormGroup>
+                            <Label for="name">Name</Label>
+                            <Input type="text" name="name" id="name" value={productModel.name} onChange={(e) => onChangeHandler(e.target.name, e.target.value)} placeholder="Product name" />
+                        </FormGroup>
+                        <Label for="pro">Category</Label>
+                        <Input type="select" name="productTypeId" id="productTypeId" value={productModel.productTypeId} onChange={(e) => onChangeHandler(e.target.name, parseInt(e.target.value))}>
+                            <option value={0}>None</option>
+                            {props.categories.map((category) =>
+                                <option value={category.id} key={category.id}>{category.name}</option>)
+                            }
+                        </Input>
+                        <FormGroup>
+                            <Label for="name">Details</Label>
+                            <Input type="textarea" name="details" id="details" value={productModel.details} onChange={(e) => onChangeHandler(e.target.name, e.target.value)} placeholder="Product details" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="name">Price</Label>
+                            <Input type="number" name="price" id="price" value={productModel.price} onChange={(e) => onChangeHandler(e.target.name, e.target.value)} placeholder="Product price" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="name">Weight</Label>
+                            <Input type="number" name="weight" id="weight" value={productModel.weight} onChange={(e) => onChangeHandler(e.target.name, e.target.value)} placeholder="Product weight" />
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input name="isLiquid" value={productModel.isLiquid} onChange={(e) => onChangeHandler(e.target.name, e.target.checked)} type="checkbox" />{' '}
+                                Is liquild
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input name="isHidden" value={productModel.isHidden} onChange={(e) => onChangeHandler(e.target.name, e.target.checked)} type="checkbox" />{' '}
+                                Is hidden
+                            </Label>
+                        </FormGroup>
+                    </Form>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="secondary" onClick={toggle}>Cancel</Button>{' '}
+                    <Button color="primary" onClick={toggle}>{isCreate ? "Create" : "Update"}</Button>
+                </ModalFooter>
+            </Modal>
+
+        </>
+    )
+}
